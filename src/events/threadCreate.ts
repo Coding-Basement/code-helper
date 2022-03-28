7a79f6105352a38a1a9621e6cc91b0b60db7defb
Mags007 <magnus.bache@outlook.de>
2022-03-28 21:32:18 +0200

    deleted threadCreate.ts

--- src/events/threadCreate.ts
+++
@@ -1,8 +0,0 @@
-import { updateThreadNotifyMessage } from '../modules/codingtreads';
-import { Event } from '../Structures/Event';
-
-export default new Event('threadCreate', async (thread) => {
-   setTimeout(() => {
-      updateThreadNotifyMessage();
-   }, 1000);
-});
